ceee322c1c3dd57d42a0e328b5cf600a588b9fed
khanghoang <khanghoang1702@gmail.com>
2021-01-11 11:09:37 +0700

    a

---
+++ build/web/WEB-INF/classes/resouces.res
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+META-INF/eclipselink-orm.xml
+META-INF/orm.xml
+=======
+META-INF/orm.xml
+META-INF/eclipselink-orm.xml
+>>>>>>> 216c72d44088304b0381e33af4a5489e361b2bd2
---
+++ src/java/dao/ItemDB.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+import javax.persistence.*;
+
+import model.Items;
+
+/**
+ *
+ * @author pc
+ */
+public class ItemDB {
+
+    public static List<Items> getAllItems() throws Exception {
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("item");
+        EntityManager entityManager = factory.createEntityManager();
+        String sql = "SELECT item  FROM Items item";
+        Query query = entityManager.createQuery(sql);
+        List<Items> listItems = (List<Items>) query.getResultList();
+        entityManager.close();
+
+        return listItems;
+
+    }
+}
